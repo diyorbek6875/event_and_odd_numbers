6f9bde563bcc5f48cb51fe1d9c359b2164b51404
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-04-07 11:22:18 +0500

    1

--- is_even.py
+++ is_even.py
@@ -1,3 +1,6 @@
 #Create a variable "number" and assign it a integer value.
 
 #Print 1 if the number is even, otherwise print 0.
+number=int(input("raqan kiriting "))
+number=(number%2+1)%2
+print(number)
